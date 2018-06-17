dc99d5662708ee6aded9853741b9db574bbc330d
Daniel Smith <danielsmith.2796@googlemail.com>
2018-06-17 15:39:10 +0100

    initial commit, server project setup

---
+++ server-side/src/main/java/com/QA/personal/project/springbootangularcrudapplication/SpringbootAngularCrudApplication.java
@@ -0,0 +1,12 @@
+package com.QA.personal.project.springbootangularcrudapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularCrudApplication.class, args);
+	}
+}
